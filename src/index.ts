c03f60ea23c0da83c11c71eb68dd4a343d09b46b
Ash Lewis <ashlewis1001@gmail.com>
2023-09-15 08:47:04 +0100

    removed console log and fixed syntax error

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,15 @@ import dotenv from "dotenv";
 import cors from "cors";
 const https = require("https");
 const fs = require("fs");
-const timeout = require('connect-timeout'); //express v4
+const timeout = require("connect-timeout"); //express v4
 const path = require("path");
-const https_options = {
-	ca: fs.readFileSync(path.resolve(__dirname, "../", "ssl", "ca_bundle.crt")),
-	key: fs.readFileSync(path.resolve(__dirname, "../", "ssl", "private.key")),
-	cert: fs.readFileSync(
-		path.resolve(__dirname, "../", "ssl", "certificate.crt")
-	),
-};
+// const https_options = {
+// 	ca: fs.readFileSync(path.resolve(__dirname, "../", "ssl", "ca_bundle.crt")),
+// 	key: fs.readFileSync(path.resolve(__dirname, "../", "ssl", "private.key")),
+// 	cert: fs.readFileSync(
+// 		path.resolve(__dirname, "../", "ssl", "certificate.crt")
+// 	),
+// };
 
 //routes
 import generateCode from "./routes/generateCode";
@@ -28,14 +28,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const haltOnTimedout = (req: any, res: any, next: any) => {
-	if (!req.timedout) next();
+  if (!req.timedout) next();
 };
 
 const corsOptions = {
-	origin: "*",
-	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-	preflightContinue: false,
-	optionsSuccessStatus: 204,
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
@@ -45,22 +45,22 @@ app.use(timeout(120000));
 app.use(haltOnTimedout);
 
 app.use(function (req, res, next) {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader(
-		"Access-Control-Allow-Methods",
-		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
-	);
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"X-Requested-With,content-type"
-	);
-	next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  next();
 });
 
 app.use(express.json());
 
 app.get("/status", (req, res) => {
-	res.status(200).json({ status: "ok" });
+  res.status(200).json({ status: "ok" });
 });
 
 app.use(generateCode);
@@ -81,7 +81,7 @@ const secure_port = 1337;
 
 app.listen(port, () => console.log(`Sever is running port ${port} ...`));
 
-const sslServer = https.createServer(https_options, app);
-sslServer.listen(secure_port, () => {
-	console.log(`Sever is running port ${secure_port} 🚀🦾🔑`);
-});
+// const sslServer = https.createServer(https_options, app);
+// sslServer.listen(secure_port, () => {
+// 	console.log(`Sever is running port ${secure_port} 🚀🦾🔑`);
+//});
